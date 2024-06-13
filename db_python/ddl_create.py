ed8b5945aaa734b2e5a16d2fd196914a884c3916
redsonlopez <redsonlopez@gmail.com>
2024-06-13 20:32:36 -0300

    13/06/24

---
+++ db_python/ddl_create.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+conn= sqlite3.connect('redDB.db')
+print(type(conn))
+
+# Data Definition Language; Data Manipulation Language; Data Control Language
+ddl_create= """
+CREATE TABLE IF NOT EXISTS fornecedor(
+    id_fornecedor INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    nome_fornecedor TEXT NOT NULL,
+    cnpj VARCHAR (18) NOT NULL,
+    cidade TEXT,
+    estado VARCHAR (2) NOT NULL,
+    cep VARCHAR (9) NOT NULL,
+    data_cadastro DATE NOT NULL
+);
+"""
+
+cursor= conn.cursor()
+cursor.execute(ddl_create)
+print(type(cursor))
+print("Tabela criada")
+print("Descrição do cursor: ", cursor.description)
+print("Linhas afetadas: ", cursor.rowcount)
+
+cursor.close()
+conn.close()
+
---
+++ db_python/dml_create.py
@@ -0,0 +1,23 @@
+# Data Definition Language; Data Manipulation Language; Data Control Language
+
+import sqlite3
+
+conn= sqlite3.connect('redDB.db')
+cursor= conn.cursor()
+
+cursor.execute("""
+INSERT INTO fornecedor (nome_fornecedor, cnpj, cidade, estado, cep, data_cadastro)
+VALUES ('Empresa A', '11.111.111/1111-11', 'São Paulo', 'SP', '11111-111', '2020-01-01')
+""")
+
+cursor.execute("""
+INSERT INTO fornecedor (nome_fornecedor, cnpj, cidade, estado, cep, data_cadastro)
+VALUES ('Empresa B', '22.222.222/2222-22', 'Rio de Janeiro', 'RJ', '22222-222', '2020-01-01')
+""")
+
+cursor.execute("""
+INSERT INTO fornecedor (nome_fornecedor, cnpj, cidade, estado, cep, data_cadastro)
+VALUES ('Empresa C', '33.333.333/3333-33', 'Curitiba', 'PR', '33333-333', '2020-01-01')
+""")
+
+

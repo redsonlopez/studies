769b26c75d587e3f54ecdca71810bfe625d2fddf
redsonlopez <redsonlopez@gmail.com>
2024-07-01 17:59:41 -0300

    0107

--- db_game/dml_read.py
+++ db_game/dml_read.py
@@ -3,7 +3,7 @@ import sqlite3
 conn= sqlite3.connect('redland.db')
 cursor= conn.cursor()
 
-cursor.execute("SELECT * FROM personagem")
+cursor.execute("SELECT * FROM personagem WHERE titulo LIKE '%monarca%'")
 resultado= cursor.fetchall()
 
 for linha in resultado:

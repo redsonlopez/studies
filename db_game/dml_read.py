f0423c8b8579e13dd363576539466b4f7ff50067
redsonlopez <redsonlopez@gmail.com>
2024-07-03 21:51:46 -0300

    0307

--- db_game/dml_read.py
+++ db_game/dml_read.py
@@ -3,8 +3,28 @@ import sqlite3
 conn= sqlite3.connect('redland.db')
 cursor= conn.cursor()
 
-cursor.execute("SELECT * FROM personagem WHERE titulo LIKE '%monarca%'")
+cursor.execute("""
+SELECT 
+    personagem.nome AS Nome, 
+    personagem.titulo AS Titulo, 
+    personagem.nivel AS Nivel, 
+    personagem.status AS Status, 
+    mapa.nome AS Habitacao, 
+    mapa.reino AS Reino
+FROM 
+    personagem
+JOIN 
+    mapa ON personagem.idhabitacao = mapa.idlocal
+WHERE
+    Reino LIKE '%Gallionne%'
+ORDER
+    BY Habitacao;
+""")
+
+colunas = [descricao[0] for descricao in cursor.description]
 resultado= cursor.fetchall()
+
+print(colunas)
 
 for linha in resultado:
     print(linha)

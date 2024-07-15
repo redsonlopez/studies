e45775cada0e5a41b2b06309328bebf885334105
redsonlopez <redsonlopez@gmail.com>
2024-07-15 14:04:16 -0300

    1407

--- a_python/ds_stack.py
+++ a_python/ds_stack.py
@@ -1,14 +1,6 @@
 class Pilha:
-    """permite a criação de uma pilha"""
     def __init__(self):
         self.itens= []
     def __repr__(self):
         return str(self.itens)
-    def empilha(self, valor):
-        """adiciona itens a pilha"""
-        self.itens.append(valor)
-    def desempilha(self):
-        assert self.itens, "Erro: pilha vazia."
-        # modifica o valor do topo
-        return self.itens.pop()
 
